5703d1b036b824f7656aac1fb5d2c4ccbde89a0d
John <johnmulvey48@yahoo.com>
2022-01-06 11:43:46 -0500

    git@github.com:Jmulvez/phase-1-practice-dog-ceo.git

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+fetch('https://dog.ceo/api/breeds/image/random/4') 
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        appendData(data);
+    });
+
+function appendData(data) {
+    const dogs = document.getElementById('dog-image-container');
+    for (const i = 0; i < data.length; i++) {
+        const div = document.createElement("div");
+        div.innerHTML = 'dog: ' +data[i].name;
+    }
+};
+
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(breedData) {
+        appendData('ul')
+    });
